88c30005d2e61d9e408977af139de4fdc3beaba6
yoan ante <yoanante@gmail.com>
2018-09-09 01:14:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+//appends the .target class to the end of #app effectively pulling it out of #nested
+function nestedTarget(){
+  getFirstSelector('#app').appendChild(getFirstSelector('.target'));
+}
+
+function increaseRankBy(n){
+ var ul = document.getElementsByClassName('ranked-list');
+ var list1 = ul[0].getElementsByTagName('li');
+ var list2 = ul[1].getElementsByTagName('li');
+ for (var i = 0; i < list1.length; i++){
+   list1[i].innerText = parseInt(list1[i].innerText)+n;
+ }
+ for (i = 0; i < list2.length; i++){
+   list2[i].innerText = parseInt(list2[i].innerText)+n;
+ }
+} 
+
+//removes the deepest child in #grand-node and appends it to #app
+function deepestChild(){
+var grandNode = document.getElementById('grand-node')
+var allDivs = grandNode.querySelectorAll('div')
+var theDeepestDiv = allDivs[allDivs.length-1]
+document.getElementById('app').appendChild(theDeepestDiv)
+}
