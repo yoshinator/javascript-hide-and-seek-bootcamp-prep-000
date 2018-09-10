c859946b28168f6e3d269018b60d5c8f5d9d8180
yoan ante <yoanante@gmail.com>
2018-09-10 22:33:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,17 +8,20 @@ function nestedTarget(){
   getFirstSelector('#app').appendChild(getFirstSelector('.target'));
 }
 
-function increaseRankBy(n){
- var ul = document.getElementsByClassName('ranked-list');
- var list1 = ul[0].getElementsByTagName('li');
- var list2 = ul[1].getElementsByTagName('li');
- for (var i = 0; i < list1.length; i++){
-   list1[i].innerText = parseInt(list1[i].innerText)+n;
- }
- for (i = 0; i < list2.length; i++){
-   list2[i].innerText = parseInt(list2[i].innerText)+n;
- }
-} 
+// function increaseRankBy(n){
+// var ul = document.getElementsByClassName('ranked-list');
+// var list1 = ul[0].getElementsByTagName('li');
+// var list2 = ul[1].getElementsByTagName('li');
+// for (var i = 0; i < list1.length; i++){
+//   list1[i].innerText = parseInt(list1[i].innerText)+n;
+// }
+// for (i = 0; i < list2.length; i++){
+//   list2[i].innerText = parseInt(list2[i].innerText)+n;
+// }
+// } 
+
+
+
 
 //removes the deepest child in #grand-node and appends it to #app
 function deepestChild(){
